066505ab64ebf68d677919470a968358a214a5dc
Boris <89937871+Musnik52@users.noreply.github.com>
2023-08-16 00:27:54 +0300

    Add files via upload

--- 00-MyHiddenPassword/script.js
+++ 00-MyHiddenPassword/script.js
@@ -1,10 +1,27 @@
 const icon = document.querySelector("#icon");
 const password = document.querySelector("#password");
+const inputBox = document.querySelector(".input-box");
+const msg = document.querySelector("#msg");
+const strength = document.querySelector("#strength");
 
 const passToggle = () => {
   password.type === "password"
-    ? (password.type = "text") && (icon.src = 'eye-open.png')
-    : (password.type = "password") && (icon.src = 'eye-close.png');
+    ? (password.type = "text") && (icon.src = "eye-open.png")
+    : (password.type = "password") && (icon.src = "eye-close.png");
+};
+
+const passwordStrength = () => {
+  msg.style.display = password.value.length ? "block" : "none";
+
+  password.value.length < 4
+    ? (strength.innerHTML = "Weak") &&
+      (msg.style.color = inputBox.style.borderColor = "#ff5925")
+    : password.value.length >= 4 && password.value.length < 8
+    ? (strength.innerHTML = "Normal") &&
+      (msg.style.color = inputBox.style.borderColor = "yellow")
+    : (strength.innerHTML = "Strong") &&
+      (msg.style.color = inputBox.style.borderColor = "#26d730");
 };
 
 icon.addEventListener("click", passToggle);
+password.addEventListener("input", passwordStrength);
